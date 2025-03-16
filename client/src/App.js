c86837ec8be4467eef9535ad4ba296aa23f3428e
Mustafa <mustafaerbilici@gmail.com>
2025-03-17 00:58:23 +0300

    Refactor Convert class component to functional component

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import { Game } from './game';
 import Rooms from './pages/Rooms';
 import Home from './pages/Home';
 
+const App = () => {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path="/room/:roomName" element={<Rooms />} />
+      </Routes>
+    </div>
+  );
+};
 
-class App extends Component {
-
-
-  state = {
-    data: null
-  }
-  game = null;
-
-  start = async () => {
-    console.log(this.state.data.data);
-    this.game = new Game();
-    await this.game.init();
-    this.loop();
-  }
-
-  restart = async () => {
-    await this.game.init();
-    this.loop();
-  }
-
-  loop = () => {
-    if (!this.game.gameState) return;
-    this.game.draw();
-    this.game.update();
-    requestAnimationFrame(this.loop);
-  }
-
-  render() {
-
-    return (
-      <div className="App">
-        <Routes>
-          <Route path='/' element={<Home />}></Route >
-          <Route path="/room/:roomName" element={<Rooms />} />
-        </Routes>
-      </div >
-    );
-  }
-}
 export default App;
--- client/src/pages/Rooms.js
+++ client/src/pages/Rooms.js
@@ -1,146 +1,139 @@
-import React, { Component } from 'react';
+import React, { useState, useCallback, useRef } from 'react';
 import { CANVAS_REF, CANVAS_HEIGHT, CANVAS_WIDTH } from '../utils/constants';
 import '../styles/rooms.css';
 import { Game } from '../game';
 
-export default class Rooms extends Component {
+const Rooms = () => {
+  const [messages, setMessages] = useState([
+    { text: 'Hello', sender: 'user2' },
+  ]);
+  const [newMessage, setNewMessage] = useState('');
 
-  game = null;
+  const gameRef = useRef(null);
 
-  state = {
-    messages: [
-      { text: 'Hello', sender: 'user2' },
-    ],
-    newMessage: '',
+  const handleMessageChange = (e) => {
+    setNewMessage(e.target.value);
   };
 
-  handleMessageChange = (e) => {
-    this.setState({ newMessage: e.target.value });
-  };
-
-  handleSendMessage = () => {
-    if (this.state.newMessage.trim()) {
-      const newMessage = {
-        text: this.state.newMessage,
+  const handleSendMessage = () => {
+    if (newMessage.trim()) {
+      const newMsg = {
+        text: newMessage,
         sender: 'user1',
       };
-      this.setState((prevState) => ({
-        messages: [...prevState.messages, newMessage],
-        newMessage: '',
-      }));
+      setMessages((prevMessages) => [...prevMessages, newMsg]);
+      setNewMessage('');
     }
   };
 
+  const loop = useCallback(() => {
+    if (!gameRef.current.gameState) return;
+    gameRef.current.draw();
+    gameRef.current.update();
+    requestAnimationFrame(loop);
+  }, []);
+
+  const start = useCallback(async () => {
+    if (gameRef.current === null) {
+      gameRef.current = new Game();
+    }
+    await gameRef.current.init();
+    loop();
+  }, [loop]);
+
+  const restart = useCallback(async () => {
+    await gameRef.current.init();
+    loop();
+  }, [loop]);
 
 
-  start = async () => {
-    if (this.game === null)
-      this.game = new Game();
-    await this.game.init();
-    this.loop();
-  }
+  return (
+    <div className='room-container'>
+      <div className="game-wrapper">
+        <div className="room-view">
+          <h3 className="room-title">Room Name</h3>
 
-  restart = async () => {
-    await this.game.init();
-    this.loop();
-  }
+          <h4 className="section-title">Players</h4>
+          <ul className="player-list">
+            <li>Player 1</li>
+            <li>Player 2</li>
+          </ul>
 
-  loop = () => {
-    if (!this.game.gameState) return;
-    this.game.draw();
-    this.game.update();
-    requestAnimationFrame(this.loop);
-  }
-
-  render() {
-    return (
-      <div className='room-container'>
-        <div className="game-wrapper">
-          <div className="room-view">
-            <h3 className="room-title">Room Name</h3>
-
-            <h4 className="section-title">Players</h4>
-            <ul className="player-list">
-              <li>Player 1</li>
-              <li>Player 2</li>
-            </ul>
-
-            <h4 className="section-title">Player Scores</h4>
-            <ul className="score-list">
-              <li>
-                <span>Player 1</span> <span className="score">200</span>
-              </li>
-              <li>
-                <span>Player 2</span> <span className="score">500</span>
-              </li>
-            </ul>
-            <div className="game-buttons">
-              <button className="game-button start-button" onClick={this.start}>START</button>
-              <button className="game-button" onClick={this.restart}>RESTART</button>
-              <button className="game-button leave-button">LEAVE</button>
-            </div>
+          <h4 className="section-title">Player Scores</h4>
+          <ul className="score-list">
+            <li>
+              <span>Player 1</span> <span className="score">200</span>
+            </li>
+            <li>
+              <span>Player 2</span> <span className="score">500</span>
+            </li>
+          </ul>
+          <div className="game-buttons">
+            <button className="game-button start-button" onClick={start}>START</button>
+            <button className="game-button" onClick={restart}>RESTART</button>
+            <button className="game-button leave-button">LEAVE</button>
           </div>
-
-
-          {/* Canvas */}
-          <div className="game-canvas-container">
-            <canvas
-              ref={CANVAS_REF}
-              height={CANVAS_HEIGHT}
-              width={CANVAS_WIDTH}
-            ></canvas>
-          </div>
-
-          {/* Chat Section */}
-          <div className="chat-wrapper">
-            <section className="chat-header"> Chat</section>
-            {/* Chat Messages */}
-            <div className="chat-messages">
-              {this.state.messages.map((message, index) => (
-                <div
-                  key={index}
-                  className={`message-item ${message.sender === 'user1' ? 'isSender' : ''}`}
-                >
-                  {message.sender !== 'user1' && (
-                    <img
-                      src="/assets/redbird-downflap.png"
-                      alt="User Profile"
-                      className="message-avatar"
-                    />
-                  )}
-                  <div className="message-text">{message.text}</div>
-                  {message.sender === 'user1' && (
-                    <img
-                      src="/assets/bluebird-downflap.png"
-                      alt="User Profile"
-                      className="message-avatar"
-                    />
-                  )}
-                </div>
-
-              ))}
-            </div>
-
-            {/* Message Input */}
-            <div className="chat-input-wrapper">
-              <input
-                type="text"
-                value={this.state.newMessage}
-                onChange={this.handleMessageChange}
-                className="chat-input"
-                placeholder="Type a message..."
-              />
-              <button
-                onClick={this.handleSendMessage}
-                className="send-message-button"
-              >
-                Send
-              </button>
-            </div>
-          </div>
-
         </div>
-      </div >
-    );
-  }
-}
+
+        {/* Canvas */}
+        <div className="game-canvas-container">
+          <canvas
+            ref={CANVAS_REF}
+            height={CANVAS_HEIGHT}
+            width={CANVAS_WIDTH}
+          ></canvas>
+        </div>
+
+        {/* Chat Section */}
+        <div className="chat-wrapper">
+          <section className="chat-header"> Chat</section>
+          {/* Chat Messages */}
+          <div className="chat-messages">
+            {messages.map((message, index) => (
+              <div
+                key={index}
+                className={`message-item ${message.sender === 'user1' ? 'isSender' : ''}`}
+              >
+                {message.sender !== 'user1' && (
+                  <img
+                    src="/assets/redbird-downflap.png"
+                    alt="User Profile"
+                    className="message-avatar"
+                  />
+                )}
+                <div className="message-text">{message.text}</div>
+                {message.sender === 'user1' && (
+                  <img
+                    src="/assets/bluebird-downflap.png"
+                    alt="User Profile"
+                    className="message-avatar"
+                  />
+                )}
+              </div>
+            ))}
+          </div>
+
+          {/* Message Input */}
+          <div className="chat-input-wrapper">
+            <input
+              type="text"
+              value={newMessage}
+              onChange={handleMessageChange}
+              className="chat-input"
+              placeholder="Type a message..."
+            />
+            <button
+              onClick={handleSendMessage}
+              className="send-message-button"
+            >
+              Send
+            </button>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Rooms;
